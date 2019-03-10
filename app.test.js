3b61224db8fb9f1818f8f4ba92a3cb925f549094
matus dubrava <matusdubrava@gmail.com>
2019-03-10 15:00:46 +0100

    added travis file and some tests

---
+++ app.test.js
@@ -0,0 +1,7 @@
+const expect = require('chai').expect;
+
+describe('just to test travis CI', () => {
+    it('should pass', () => {
+        expect(1).to.equal(1);
+    });
+});
--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const port = 5000;
 
 app.get('/', (req, res) => {
-    res.send('hello world');
+    res.send('hello world!');
 });
 
 app.listen(port, () => {
